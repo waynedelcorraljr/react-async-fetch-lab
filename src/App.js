c1b667cf4fddbe104b2ffc0d3879466d67382312
Wayne del Corral Jr <wdelcorjr@gmail.com>
2021-04-23 14:45:47 -0500

    commit tree af4f37c146db0c2a63e9b1343e844f14d66e53b5

--- src/App.js
+++ src/App.js
@@ -1,1 +1,27 @@
 // create your App component here
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    
+    state = {
+        astros: []
+    }
+    
+    render() {
+        return (
+            <div>
+                {this.state.astros.map(person => <p key={person.name}>{person.name}</p>)}
+            </div>
+        )
+    }
+
+    componentDidMount() {
+        fetch('http://api.open-notify.org/astros.json')
+        .then(resp => resp.json())
+        .then(data => {
+            this.setState({
+                astros: data.people
+            })
+        })
+    }
+}
